a6a6d66cb09756f0fc5ff30be55987e9f3d7d5d1
Manav Sharma <Manav.SHARMA@teck.com>
2022-11-01 01:52:56 -0700

    Add the Categories/Statuses Button

--- SuggestionAppLibrary/DataAccess/DbConnection.cs
+++ SuggestionAppLibrary/DataAccess/DbConnection.cs
@@ -6,27 +6,24 @@ public class DbConnection : IDbConnection
 {
    private readonly IConfiguration config;
    private readonly IMongoDatabase database;
-   private string ConnectionId = "MongoDB";
-   public string DbName { get; private set; }
+   private static string ConnectionId = "mongodb://localhost:27017";
 
+   public string DbName { get; private set; }
    public string CategoryCollectionName { get; private set; } = "categories";
    public string StatusCollectionName { get; private set; } = "statuses";
    public string UserCollectionName { get; private set; } = "users";
    public string SuggestionCollectionName { get; private set; } = "suggestions";
-
    public MongoClient Client { get; private set; }
-
    public IMongoCollection<CategoryModel> CategoryCollection { get; private set; }
    public IMongoCollection<StatusModel> StatusCollection { get; private set; }
    public IMongoCollection<UserModel> UserCollection { get; private set; }
    public IMongoCollection<SuggestionModel> SuggestionCollection { get; private set; }
 
-
-
    public DbConnection(IConfiguration config)
    {
       this.config = config;
-      Client = new MongoClient(config.GetConnectionString(ConnectionId));
+
+      Client = new MongoClient(ConnectionId);
       DbName = config["DatabaseName"];
       database = Client.GetDatabase(DbName);
 
